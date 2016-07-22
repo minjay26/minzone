899cf33e5c8dfe568f56b347df859b48410e3f0e
minjay26 <823785135@qq.com>
2016-07-22 16:33:30 +0800

    第一次提交

---
+++ min-zone-manger/src/main/java/cn/tendata/minzone/config/MvcConfig.java
@@ -0,0 +1,10 @@
+package cn.tendata.minzone.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MvcConfig {
+   
+	
+	
+}
---
+++ min-zone-webapp/src/main/java/cn/tendata/minzone/webapp/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package cn.tendata.minzone.webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MvcConfig {
+   
+	
+//	public TemplateResolver templateResolver() {
+//		
+//	}
+}
